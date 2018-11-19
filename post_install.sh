6187841c05d35b46f61f3a1af049b05036594946
Martin Wilke <miwi@ixsystems.com>
2018-11-19 12:25:30 +0800

    PR53127

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 sysrc -f /etc/rc.conf sonarr_enable="YES"
 
+#we need write permission to be able to write plugins update. #53127
+chmod 755 /usr/local/sonarr
+
 # Start the services
 service sonarr start
