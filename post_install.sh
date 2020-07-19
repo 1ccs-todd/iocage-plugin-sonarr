c9b94800b2a34c6f4869cb8012482a567d1c2dff
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-19 23:10:02 +0200

    More plugin compatibility patches

--- post_install.sh
+++ post_install.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-sysrc -f /etc/rc.conf sonarr_enable="YES"
+#!/bin/bash
 
-#62991
-chown -R sonarr:sonarr /usr/local/share/sonarr
+pw user add sonarr -c sonarr -u 351 -d /nonexistent -s /usr/bin/nologin
 
-#we need write permission to be able to write plugins update. #53127
-chmod 755 /usr/local/sonarr
+fetch http://download.sonarr.tv/v2/master/mono/NzbDrone.master.tar.gz -o /usr/local/share
+tar -xzvf /usr/local/share/NzbDrone.master.tar.gz -C /usr/local/share
+rm /usr/local/share/NzbDrone.master.tar.gz
 
-# Start the services
-service sonarr start
+chmod u+x /usr/local/etc/rc.d/sonarr
+sysrc "sonarr_enable=YES"
